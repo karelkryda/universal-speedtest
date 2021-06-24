5df1a1c524f3c57a47b7eb45bde44286053c3efb
Karel Krýda <59953812+karelkryda@users.noreply.github.com>
2021-06-24 23:36:49 +0200

    Package rename + more speed tests + Release v1.0.0

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,5 +1,37 @@
+import puppeteer = require("puppeteer");
+import chromium = require("chromium");
 import configureMeasurements from "convert-units";
 import allMeasures from "convert-units/lib/cjs/definitions";
+
+/**
+ * Function to lauch Puppeteer browser.
+ * 
+ * This function will pick correct ```executablePath``` for Linux systems based on ARM processors
+ * @param executablePath Path to the Chrome startup file
+ * @returns ```Puppeteer browser```
+ */
+export async function launchBrowser(executablePath: string): Promise<puppeteer.Browser> {
+    try {
+        //Launches Puppeteer browser for Windows and MacOS
+        return await puppeteer.launch({ executablePath: chromium.path, args: ["--no-sandbox"] });
+    } catch (error) {
+        try {
+            //Launches the Puppeteer browser for Linux systems based on ARM processors
+            return await puppeteer.launch({ executablePath: "/usr/bin/chromium-browser", args: ["--no-sandbox"] });
+        } catch (error) {
+            if (executablePath) {
+                try {
+                    //Launches the Puppeteer browser using a user-specified path
+                    return await puppeteer.launch({ executablePath, args: ["--no-sandbox"] });
+                } catch (error) {
+                    throw new Error("Unable to launch Puppeteer. See possible solutions to the problem at https://github.com/karelkryda/universal-speedtest/wiki/Troubleshooting.");
+                }
+            }
+            else
+                throw new Error("Unable to launch Puppeteer. See possible solutions to the problem at https://github.com/karelkryda/universal-speedtest/wiki/Troubleshooting.");
+        }
+    }
+}
 
 /**
  * Function to convert a unit to another unit.
