85897f9101435cd0402b3ffdd15279045e93c5e3
Karel Krýda <karel.kryda@gmail.com>
2022-04-10 14:51:42 +0200

    Ups...forgot to bump version in agent header

--- src/helpers/UrllibHelper.ts
+++ src/helpers/UrllibHelper.ts
@@ -16,7 +16,7 @@ import {XMLValidator} from "fast-xml-parser";
  * @returns Promise
  */
 export function createRequest(url: string, headers: IncomingHttpHeaders, secure = true, data = {}, bump = "0", timeout = 10, withBump = false, urllibOptions: RequestOptions = {}): Promise<HttpClientResponse<unknown>> {
-    headers["user-agent"] = "Mozilla/5.0 (" + process.platform + "; U; " + process.arch + "; en-us) TypeScript/" + process.version + " (KHTML, like Gecko) UniversalSpeedTest/2.0.3";
+    headers["user-agent"] = "Mozilla/5.0 (" + process.platform + "; U; " + process.arch + "; en-us) TypeScript/" + process.version + " (KHTML, like Gecko) UniversalSpeedTest/2.0.4";
     headers["cache-control"] = "no-cache";
 
     return request(((url[0] == ":") ? (secure) ? "https" : "http" : "") + url + ((withBump) ? (((url.includes("?")) ? "&" : "?") + "x=" + performance.now() + bump) : ""), {
